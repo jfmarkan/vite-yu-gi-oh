d822601eedc69e7f9baa1b66438e9a07b3ecd60f
jfmarkan <jfmarkan@gmail.com>
2023-06-20 13:23:26 +0200

    dynamically filled the filter dropdown

--- src/components/ActionBar.vue
+++ src/components/ActionBar.vue
@@ -1,20 +1,32 @@
 <template>
     <div class="container py-3">
-        <select name="type" id="type-select">
-            <option value="Alien">Alien</option>
+        <select name="type" id="type-select" >
+            <option v-for="filter in filtersList" :value="filter.archetype_name">{{filter.archetype_name}}</option>
         </select>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     name: 'ActionBar',
     data(){
         return {
-
+            filterUrl : 'https://db.ygoprodeck.com/api/v7/archetypes.php',
+            filtersList:[],
         }
     },
-}
+    created(){
+        axios.get(this.filterUrl)
+            .then((response) => {
+                this.filtersList = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+        },
+    };
 </script>
 
 <style lang="scss">
